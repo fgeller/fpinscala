d4382c293ed9ea2bfbfc7ba23a3235535be64359
Felix Geller <fgeller@gmail.com>
2013-11-09 12:45:09 +1300

    List#tail implementation

--- exercises/src/main/scala/fpinscala/datastructures/List.scala
+++ exercises/src/main/scala/fpinscala/datastructures/List.scala
@@ -46,7 +46,10 @@ object List { // `List` companion object. Contains functions for creating and wo
   def product2(l: List[Double]) =
     foldRight(l, 1.0)(_ * _) // `_ * _` is more concise notation for `(x,y) => x * y`, see sidebar
 
-  def tail[A](l: List[A]): List[A] = sys.error("todo")
+  def tail[A](l: List[A]): List[A] = l match {
+    case Cons(h, t) ⇒ t
+    case _          ⇒ throw new UnsupportedOperationException("tail of empty list")
+  }
 
   def setHead[A](l: List[A])(h: A): List[A] = sys.error("todo")
 
